05538612e6bd4111b0ca2311a0abd0bc43878a15
preston <FX_schwartz505@yahoo.com>
2016-03-05 18:53:05 -0600

    added overall funtionality

--- app.js
+++ app.js
@@ -1,39 +1,36 @@
 var app = angular.module('app', []);
 
 app.controller('MainCtrl', function($scope) {
-// 	$scope.taxNum = 8.517;
+	$scope.taxRate = 1.08517;
 	$scope.items = [
-		{id: 1, text: "Frozen Pizza",price:7.98,quantity:1,priceWtax:$scope.tax = function(price,quantity) {
-			$scope.items.price = price;
-			$scope.items.quantity = quantity;
-			($scope.items.price * $scope.items.quantity)/**$scope.taxnum*/}, bought:true},
-		{id: 2, text: "Ice Cream",price:5.63,quantity:4, priceWtax:$scope.tax = function(price,quantity) {
-			($scope.items.price * $scope.items.quantity)/**$scope.taxnum*/}, bought:true},
-		{id: 3, text: "Peaches",price:2.89,quantity:2, priceWtax:$scope.tax = function(price,quantity) {
-			($scope.items.price * $scope.items.quantity)/**$scope.taxnum*/}, bought:true}
+		{id:1, text:'Frozen Pizza',	price:7.98,	quantity:1},
+		{id:2, text:'Ice Cream',	price:5.63,	quantity:4},
+		{id:3, text:'Peaches',		price:2.89,	quantity:2}
 	];
+
+	$scope.nextId = $scope.items.length + 1;
 	
-	
-	$scope.nextId = $scope.items.length + 1	 
-	
-	$scope.clearBought = function() {
-		$scope.items = _.filter($sope.items, function(item) {
-			return !item.bought;
-		});
-	}
 	
 	$scope.addItem = function() {
-		$scope.items.push({text: $scope.itemEntry, price:$scope.priceEntry, quantity:$scope.quantityEntry, priceWtax:$scope.tax, bought: false, id: ($scope.nextId)});
+		$scope.items.push({text: $scope.itemEntry, price:$scope.priceEntry, quantity:$scope.quantityEntry, id: ($scope.nextId)});
 		$scope.nextId++;
-		$scope.priceEntry = '';
-		$scope.quantityEntry = '';
+		$scope.priceEntry = 0;
+		$scope.quantityEntry = 0;
 		$scope.itemEntry = '';
-	}
-	
-	$scope.isBought = function(bought) {
-		return (bought) ? 'bought' : 'not-bought';
-	}
+	};
+
+	$scope.buyItem = function(item) {
+		item.bought = !item.bought;
+	};
+
+	$scope.deleteItem = function(item) {
+		var index = $scope.items.indexOf(item);
+		$scope.items.splice(index,1);
+	};
+
+	$scope.isItemBought = function(item) {
+		return (item.bought) ? 'bought' : 'not-bought';
+	};
 	
 
-	
-	});
+});
